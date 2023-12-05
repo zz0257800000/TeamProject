ae3872494997c74b88384440443eec768d3629cf
zz0257800000 <zz0257800000@gmail.com>
2023-12-05 17:11:13 +0800

    511

--- src/App.vue
+++ src/App.vue
@@ -9,24 +9,10 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <navbar />
 
-<div class="page">
   <RouterView />
-<<<<<<< HEAD
-<<<<<<< HEAD
-</div>
-=======
   <navbar1/>
 
->>>>>>> 9f890cc (445)
-=======
-  <navbar1/>
-
->>>>>>> 137faf1a8a710d95b1601e636001d6df529ebd15
 
 </template>
 
-<style scoped>
-.page{
-  margin-top: 20px;
-}
-</style>
+<style scoped></style>
