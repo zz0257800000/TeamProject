fdab0ca8eef297afb206db394235a201876ec2a1
。 <146692624+mm7274a00@users.noreply.github.com>
2023-12-07 03:15:14 +0800

    1206

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -26,6 +26,33 @@ export default{
 
 <template>
   <div class="mainshow">
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="../../public/stock1.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="../../public/stock1.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="../../public/stock1.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+    
     <div class="productAll">
       <div class="product" style="width: 18rem ; height:18rem;" v-for="(item, index) in productArr" :key="index">
         <RouterLink class="btn" to="UserPage/productPage">
@@ -45,10 +72,16 @@ export default{
   border: 0px solid rgb(255, 0, 0);
   height: 100%;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   background-color: rgb(255, 255, 255); /* 添加背景颜色 */
 
+.carousel{
+  width: 70vw;
+  margin-bottom: 20px;
+  background-color: aqua;
+}
 .productAll {
   border: 1px solid #ddd;
   width: 70vw;
