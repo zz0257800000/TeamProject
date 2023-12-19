15146ffdfb9c082afaa5f6431686a52bfac95ea3
jimmy038 <qtp0110416@gmail.com>
2023-12-19 11:04:08 +0800

    1104

--- src/views/UserPage/loginPage.vue
+++ src/views/UserPage/loginPage.vue
@@ -1,125 +1,110 @@
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
-    data() {
-        return {
-            showLogin: true,
-            email: "",
-            password: "",
-        };
-    },
-    methods: {
-        switchToLogin() {
-            this.showLogin = true;
-        },
-        switchToSignup() {
-            this.showLogin = false;
-        },
-        LoginUser() {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    LoginUser() {
       const userData = {
         email: this.email,
         password: this.password,
       };
       // 使用 Axios 發送 POST 請求
-      axios
-        .post("http://localhost:8080/user/login", userData)
+      axios.post("http://localhost:8080/user/login", userData)
         .then((response) => {
           console.log(response.data);
-           // 登录成功后导航回首页
-      this.$router.push('/');
+          // 登入成功後導向首頁
+          this.$router.push("/");
         })
         .catch((error) => {
           console.error(error);
           // 處理錯誤，顯示錯誤訊息等
         });
     },
-    },
+  },
 };
 </script>
 
 <template>
- <div class="secondtitle2">
-      <h3>
-      </h3>
-      <h6>
-        <RouterLink class="btn" to="/"> Home</RouterLink> > <a href="">登入</a>
-      </h6>
-
-    </div>
-<div class="mainLoginShow">
-  
-  
+  <div class="secondtitle2">
+    <h3></h3>
+    <h6>
+      <RouterLink class="btn" to="/"> Home</RouterLink> > <a href="">登入</a>
+    </h6>
+  </div>
+  <div class="mainLoginShow">
     <div class="leftShow">
-      <h1> <i class="fa-solid fa-shrimp"><b> 呱皮皮蝦</b> </i>
-        </h1>
+      <h1>
+        <i class="fa-solid fa-shrimp"><b> 呱皮皮蝦</b> </i>
+      </h1>
     </div>
     <div class="login-box">
-        <div class="lb-header">
-          <h3>歡迎回來</h3>
+      <div class="lb-header">
+        <h3>歡迎回來</h3>
+      </div>
+      <br />
 
+      <form class="email-login">
+        <div class="u-form-group">
+          <i class="fa-solid fa-user"></i> &nbsp;<input type="email" placeholder="Email" v-model="email"/>
         </div>
-        <br>
-        
-        <form class="email-login">
-            <div class="u-form-group">
-              <i class="fa-solid fa-user"></i> &nbsp;<input type="email" placeholder="Email" v-model="email"/>
-            </div>
-            <div class="u-form-group">
-              <i class="fa-solid fa-lock"></i> &nbsp; <input type="password" placeholder="Password" v-model="password"/>
-            </div>
-          <div class="passwordright">
-            <h1></h1>
-            <RouterLink class="btn" to="/UserPage/forgotPasswordPage">忘記密碼</RouterLink>
-          </div>
-            <div class="u-form-group">
-                <!-- <RouterLink class="loginBtn" to="/">Login</RouterLink> -->
-                <button @click="LoginUser" class="loginBtn" type="button">登入</button>
-            </div>
-            <div class="u-form-group">
-             
-                <RouterLink class="signBtn" to="/UserPage/signUp">註冊</RouterLink>
-
-            </div>
-        </form>
-        <div class="social-login">
-            <a href="#">
-              <i class="fa-brands fa-facebook"></i>
-                Login  facebook
-            </a>
-            <a href="#">
-              <i class="fa-brands fa-google"></i>                Login  Google
-            </a>
+        <div class="u-form-group">
+          <i class="fa-solid fa-lock"></i> &nbsp;
+          <input type="password" placeholder="Password" v-model="password" />
         </div>
+        <div class="passwordright">
+          <h1></h1>
+          <RouterLink class="btn" to="/UserPage/forgotPasswordPage"
+            >忘記密碼</RouterLink
+          >
+        </div>
+        <div class="u-form-group">
+          <!-- <RouterLink class="loginBtn" to="/">Login</RouterLink> -->
+          <button @click="LoginUser" class="loginBtn" type="button">
+            登入
+          </button>
+        </div>
+        <div class="u-form-group">
+          <RouterLink class="signBtn" to="/UserPage/signUp">註冊</RouterLink>
+        </div>
+      </form>
+      <div class="social-login">
+        <a href="#">
+          <i class="fa-brands fa-facebook"></i>
+          Login facebook
+        </a>
+        <a href="#"> <i class="fa-brands fa-google"></i> Login Google </a>
+      </div>
     </div>
-    </div>
+  </div>
 </template>
-  
- 
-  
+
 <style lang="scss" scoped>
- .secondtitle2 {
-    justify-content: space-between;
-    display: flex;
-    align-items: center;
-    border: 0px solid rgb(255, 0, 0);
-    width: 95vw;
-    height: 15vh;
+.secondtitle2 {
+  justify-content: space-between;
+  display: flex;
+  align-items: center;
+  border: 0px solid rgb(255, 0, 0);
+  width: 95vw;
+  height: 15vh;
 
-    a {
+  a {
+    border-radius: 10px;
+    padding: 5px;
+    transition: all 0.5s ease;
+    text-decoration: none;
+    color: black;
 
-      border-radius: 10px;
-      padding: 5px;
-      transition: all 0.5s ease;
-      text-decoration: none;
-      color: black;
-
-      &:hover {
-        color: red;
-        background-color: rgba(118, 118, 117, 0.5);
-      }
+    &:hover {
+      color: red;
+      background-color: rgba(118, 118, 117, 0.5);
     }
-
   }
+}
 .mainLoginShow {
   border: 0px solid red;
   height: 80vh;
@@ -133,7 +118,6 @@ export default {
     width: 90vw;
 
     a {
-
       border-radius: 10px;
       padding: 5px;
       transition: all 0.5s ease;
@@ -145,7 +129,6 @@ export default {
         background-color: rgba(118, 118, 117, 0.5);
       }
     }
-
   }
 
   .leftShow {
@@ -171,7 +154,7 @@ export default {
 
   .lb-header {
     display: flex;
-    
+
     position: relative;
     color: #00415d;
     margin: 5px 5px 10px 5px;
@@ -229,11 +212,11 @@ export default {
       }
 
       &:first-child {
-        background-color: #49639F;
+        background-color: #49639f;
       }
 
       &:last-child {
-        background-color: #DF4A32;
+        background-color: #df4a32;
       }
     }
   }
@@ -269,11 +252,11 @@ export default {
       }
     }
   }
-  .passwordright{
+  .passwordright {
     display: flex;
     justify-content: space-between;
     border: 0px solid rgb(0, 0, 0);
-width: 27vw;
+    width: 27vw;
   }
 
   .loginBtn {
@@ -283,7 +266,7 @@ width: 27vw;
     font-size: 20px;
     border-radius: 4px;
     text-decoration: none;
-    width: calc(75% + 20px);  // 考慮到 padding
+    width: calc(75% + 20px); // 考慮到 padding
     display: block;
     margin: 0 auto;
     padding: 10px;
@@ -297,14 +280,11 @@ width: 27vw;
     font-size: 20px;
     border-radius: 4px;
     text-decoration: none;
-    width: calc(75% + 20px);  // 考慮到 padding
+    width: calc(75% + 20px); // 考慮到 padding
     display: block;
     margin: 0 auto;
     padding: 10px;
     margin-top: 20px;
   }
 }
-
-
 </style>
-  
