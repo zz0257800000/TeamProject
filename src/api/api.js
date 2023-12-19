d91b8707da7839c645a8a70710788b55b058a775
mm7274a00 <mm7274a00@gmail.com>
2023-12-19 15:19:38 +0800

    更正api.js

--- src/api/api.js
+++ src/api/api.js
@@ -31,8 +31,57 @@ const login = (req) => {
     });
 };
 
+//加入購物車
+const cartCreat = (req) => {
+  return axios({
+    method: "post",
+    url: `http://localhost:8080/cart/create`,
+    data: req,
+  })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((e) => {
+      throw e;
+    });
+};
+
+//刪除購物車
+// 刪除購物車
+const cartDelete = (cartId) => {
+  return axios({
+    method: "post",
+    url: `http://localhost:8080/cart/delete?id=${cartId}`,
+  })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((e) => {
+      throw e;
+    });
+};
+
+
+//尋找購物車
+const getCartInfoByUserId = (userId) => {
+  return axios({
+    method: "get",
+    url: `http://localhost:8080/cart/get/user_id?id=${userId}`,
+    data: userId,
+  })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((e) => {
+      throw e;
+    });
+};
+
 
 export default {
   signUp,
   login,
+  cartCreat,
+  cartDelete,
+  getCartInfoByUserId,
 };
